18858b7733e6d52ff7d5db6382bbb55fa7ef6cb1
fastroware <mukhlisin.dev@gmail.com>
2026-07-13 10:36:36 +0700

    enabling userfactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,78 +1,78 @@
 <?php
 
-// namespace Database\Factories;
+namespace Database\Factories;
 
-// use App\Models\Personil;
-// use App\Models\User;
-// use Illuminate\Database\Eloquent\Factories\Factory;
-// use Illuminate\Support\Facades\Hash;
-// use Illuminate\Support\Str;
+use App\Models\Personil;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
-// /**
-//  * @extends Factory<User>
-//  */
-// class UserFactory extends Factory
-// {
-//     /**
-//      * The current password being used by the factory.
-//      */
-//     protected static ?string $password;
+/**
+ * @extends Factory<User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
 
-//     /**
-//      * Define the model's default state.
-//      *
-//      * @return array<string, mixed>
-//      */
-//     public function definition(): array
-//     {
-//         return [
-//             'personil_id' => Personil::factory(),
-//             'username' => $this->faker->unique()->userName(),
-//             'password' => Hash::make('password'), // default: "password"
-//             // 'role' => $this->faker->randomElement(['guru', 'murid', 'orang_tua']),
-//             'role' => $this->faker->randomElement(['guru']),
-//             'name' => fake()->name(),
-//             'email' => fake()->unique()->safeEmail(),
-//             'status' => 1,
-//             'email_verified_at' => now(),
-//             'remember_token' => Str::random(10),
-//         ];
-//     }
-//     public function admin(): static
-//     {
-//         return $this->state(fn(array $attributes) => [
-//             'role' => 'administrator',
-//         ]);
-//     }
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'personil_id' => Personil::factory(),
+            'username' => $this->faker->unique()->userName(),
+            'password' => Hash::make('password'), // default: "password"
+            // 'role' => $this->faker->randomElement(['guru', 'murid', 'orang_tua']),
+            'role' => $this->faker->randomElement(['guru']),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'status' => 1,
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ];
+    }
+    public function admin(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'role' => 'administrator',
+        ]);
+    }
 
-//     public function guru(): static
-//     {
-//         return $this->state(fn(array $attributes) => [
-//             'role' => 'guru',
-//         ]);
-//     }
+    public function guru(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'role' => 'guru',
+        ]);
+    }
 
-//     public function murid(): static
-//     {
-//         return $this->state(fn(array $attributes) => [
-//             'role' => 'murid',
-//         ]);
-//     }
+    public function murid(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'role' => 'murid',
+        ]);
+    }
 
-//     public function orangTua(): static
-//     {
-//         return $this->state(fn(array $attributes) => [
-//             'role' => 'orang_tua',
-//         ]);
-//     }
+    public function orangTua(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'role' => 'orang_tua',
+        ]);
+    }
 
-//     /**
-//      * Indicate that the model's email address should be unverified.
-//      */
-//     public function unverified(): static
-//     {
-//         return $this->state(fn(array $attributes) => [
-//             'email_verified_at' => null,
-//         ]);
-//     }
-// }
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
